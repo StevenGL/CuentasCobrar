cc84875e5b5f7d0c93bfa6412e9219bc1e32fe86
StevenGL <steven_gl@outlook.com>
2017-08-10 03:39:25 -0400

    Se agrego statoicfile

--- CuentasCobrar/settings.py
+++ CuentasCobrar/settings.py
@@ -125,5 +125,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 DATABASES['default'] =  dj_database_url.config()
+
