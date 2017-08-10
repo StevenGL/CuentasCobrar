a17ffcb30a6bdcef9ff2dd790d88929a73460c83
StevenGL <steven_gl@outlook.com>
2017-08-10 17:50:30 -0400

    Requerimets

--- apps/Cuentas/views.py
+++ apps/Cuentas/views.py
@@ -167,5 +167,7 @@ class Enviar_AsientosContables(DetailView):
         }
         r = requests.post(url, data=json.dumps(data), headers=headers)
 
+        print(r.json)
+
         return render(request, {"Envio":r})
 #     return render_to_response('Enviar.html', {'j': r},)
